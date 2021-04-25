5ddc54047d1b12fb2c84534b0aa2da841659a862
inesfmv <inesfmv.s@gmail.com>
2021-04-25 21:26:46 +0100

    desafio 2 adicionado!

--- 02dfpyines.py
+++ 02dfpyines.py
@@ -4,14 +4,30 @@
 import os 
 import ipaddress
 
-listaip=ipaddress.IPv4Network('192.168.1.0/24')
+#a) Utilizando listas e um ciclo for, escreva código que guarde numa 
+# lista o endereço IP dos primeiros 50 dispositivos numa rede. 
+# No fim, imprima a lista de IPs.
 
-for ip in listaip:
-    print(f'{ip}')
+conjuntoip=ipaddress.IPv4Network('192.168.1.0/29')
+listacinq = [ ]
+print ("alinea a")
 
-#a) Utilizando listas e um ciclo for, escreva código que guarde numa lista o endereço IP 
-# dos primeiros 50 dispositivos numa rede. No fim, imprima a lista de IPs.
+for ip in conjuntoip:
+    sinal = os.system("ping -c 1 " + str(ip) + " > /dev/null 2>&1")
+    if sinal == 0:
+        listacinq.append(f'{ip}')
+
+listacinq=listacinq[:2]
+print (listacinq)
 
 
 # b) Escreva agora uma função para imprimir um IP por linha. 
 # Para o efeito, utilize um ciclo for. A lista deverá receber a variável listaIPs como input.
+
+print ("alinea b")
+
+def imprip ():
+    for ip in listacinq:
+        print (f'{ip}')
+
+imprip ()
