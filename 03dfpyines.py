d54d765b52185693f027009122f466d5f9a2a8bc
inesfmv <inesfmv.s@gmail.com>
2021-04-26 18:55:03 +0100

    desafios atualizados

--- 03dfpyines.py
+++ 03dfpyines.py
@@ -3,7 +3,7 @@
 
 import os 
 import ipaddress
-
+from getmac import get_mac_address
 
 #a) Escreva código que, utilizando o módulo ipaddress, 
 # imprima todos os endereços IP de uma rede com o prefixo 192.168.3.x
@@ -13,8 +13,16 @@ for ip in conjuntoip:
 
 #b) O módulo os do Python permite executar funções que estejam dependentes do sistema operativo. 
 # Utilizando esse módulo e o módulo ipaddress, escreva código para executar um ping a toda a sua rede doméstica.
-localnetwork=ipaddress.ip_address
-print (localnetwork)
+rede=input("Indica qual o endereço de rede em formato CIDR (ex. 192.168.1.0/14): ")
+ipdomestico=ipaddress.IPv4Network(rede)
+listaipon = [ ]
+
+for ip in ipdomestico:
+    sinal = os.system("ping -c 1 " + str(ip) + " > /dev/null 2>&1")
+    if sinal == 0:
+        print("O endereço de IP " + (f'{ip}') + " está ligado.")
+    else:
+        print("O endereço de IP " + (f'{ip}') + " está desligado.")
 
 
 #c) Adicione agora uma linha de código para que, no início da execução do programa, 
@@ -22,7 +30,8 @@ print (localnetwork)
 user=os.getenv('LOGNAME')
 print("Nome de utilizador atualmente com sessão iniciada: ", user)
 
-
 #d) O MAC Address de um dispositivo é o identificador único da placa de rede com que este se encontra ligado. 
 # Utilize agora o módulo adicional get-mac (https://pypi.org/project/get-mac/), 
 # desenvolvido pelo MIT, para obter e imprimir o MAC Address dos dispositivos que estejam online.
+
+#maconline=get_mac_address()
